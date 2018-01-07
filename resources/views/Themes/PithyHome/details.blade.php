e097b05c5e7da0dd85e8593cce59004e1210afce
BingDing <itbing@sina.cn>
2018-01-07 14:27:39 +0800

    公共前台页面底部版权 优化详情页快速预览

--- resources/views/Themes/PithyHome/details.blade.php
+++ resources/views/Themes/PithyHome/details.blade.php
@@ -3,8 +3,8 @@
 
 		<div class="container">
 			<div class="head-text text-highlight">
-				<h1>TYPOGRAPHY</h1>
-				<p class="lead-text">H1   •   HIGHTLIGH   •   DROPCAP</p>
+				<h1>{{$artFind->title}}</h1>
+				<p class="lead-text"><i class="fa fa-at"></i></a>&nbsp;{{$artFind->admin->name}}   •   <i class="fa fa-eye"></i></a>&nbsp;{{$artFind->read_num}}</p>
 			</div>
 		</div>
 
@@ -26,7 +26,7 @@
 						<div class="col-md-10 ">
 							<div class="post-item">
 								<div class="post-item-paragraph">
-									<h2><a href="#" class="quick-read"><i class="fa fa-eye"></i></a>{{$artFind->title}}</h2>
+									<h2><a href="#" class="quick-read"><i class="fa fa-expand"></i></a></h2>
 									<p class="post-item-two-column">
 										{!! Parsedown::instance()->setMarkupEscaped(true)->text($artFind->html) !!}
 									</p>
@@ -161,17 +161,7 @@
 			</div>
 		</section>
 
-		<footer class="container-fluid footer">
-			<div class="container text-center">
-				<div class="footer-logo"><img src="/assets/img/logo-black.png" alt=""></div>
-				<p class="laread-motto">Designed for Read.</p>
-				<div class="laread-social">
-					<a href="#" class="fa fa-twitter"></a>
-					<a href="#" class="fa fa-facebook"></a>
-					<a href="#" class="fa fa-pinterest"></a>
-				</div>
-			</div>
-		</footer>
+		@include('Themes.PithyHome.Common.copyright');
 	</div>
 
 	<div id="quick-read" class="qr-white-theme">
@@ -204,15 +194,17 @@
 			</div>
 		</div>
 		<div class="quick-read-bottom">
-			<p class="qr-info">By <a href="#">Daniele Zedda</a>   •   18 February</p>
+			<p class="qr-info">By <a href="#">{{$artFind->admin->name}}</a>   •   {{date('d', strtotime($artFind->updated_at))}} {{date('F', strtotime($artFind->updated_at))}}</p>
 			<div class="qr-nav">
-				<a href="#" class="qr-prev">← PREV POST</a>
-				<a href="#" class="qr-share" tabindex="0" role="button" data-toggle="popover" data-placement="top" data-trigger="focus" data-content="<a href='#'><i class='fa fa-facebook'></i></a><a href='#'><i class='fa fa-twitter'></i></a>"><i class="fa fa-share-alt"></i></a>
+				<!--<a href="#" class="qr-prev">← PREV POST</a>-->
+				<!--<a href="#" class="qr-share" tabindex="0" role="button" data-toggle="popover" data-placement="top" data-trigger="focus" data-content="<a href='#'><i class='fa fa-facebook'></i></a><a href='#'><i class='fa fa-twitter'></i></a>"><i class="fa fa-share-alt"></i></a>-->
 				<a href="#" class="qr-comment"><i class="fa fa-comment"></i></a>
-				<a href="#" class="qr-like"><i class="fa fa-heart"></i> 34</a>
-				<a href="#" class="qr-next">NEXT POST →</a>
+				<a href="#" class="qr-like"><i class="fa fa-eye"></i> {{$artFind->read_num}}</a>
+				<!--<a href="#" class="qr-like"><i class="fa fa-heart"></i> 34</a>-->
+				<!--<a href="#" class="qr-next">NEXT POST →</a>-->
 			</div>
 		</div>
+		<!--
 		<div class="quick-read-bottom qr-bottom-2 hide">
 			<div class="qr-nav">
 				<a href="#" class="qr-prev">← PREV POST</a>
@@ -224,8 +216,10 @@
 					<a href='#'><i class='fa fa-facebook'></i></a>
 					<a href='#'><i class='fa fa-twitter'></i></a>
 				</div>
+				
 			</div>
 		</div>
+		-->
 	</div>
 
 	<!-- Bootstrap core JavaScript
--- resources/views/Themes/PithyHome/index.blade.php
+++ resources/views/Themes/PithyHome/index.blade.php
@@ -138,18 +138,7 @@
 			</div>
 		</div>
 
-		<footer class="container-fluid footer">
-			<div class="container text-center">
-				<div class="footer-logo"><img src="assets/img/logo-black.png" alt=""></div>
-				<p class="laread-motto">@if(isset($sets['FootNews'])) {{$sets['FootNews']}} @endif &nbsp;&nbsp; @if(isset($sets['RecordNum'])) {{$sets['RecordNum']}} @endif</p>
-				
-				<div class="laread-social">
-					@if(isset($sets['Facebook']))<a href="#" class="fa fa-facebook"></a>@endif
-					@if(isset($sets['Twitter']))<a href="#" class="fa fa-twitter"></a>@endif
-					@if(isset($sets['SinaWeibo']))<a href="#" class="fa fa-weibo"></a>@endif
-				</div>
-			</div>
-		</footer>
+		@include('Themes.PithyHome.Common.copyright');
 	</div>
 <!-- 文章详情 begin -->
 	<div id="quick-read" class="qr-white-theme">
